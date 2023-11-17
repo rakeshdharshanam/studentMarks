d3adc3ae3bac0af853fcfd502ffd9545235b38a2
rakesh <rakesh.dharshanam333@gmail.com>
2023-11-17 02:15:19 -0500

    resolving the @componentscan and added the spring profiles

--- src/main/java/com/rakesh/studentMicroService/StudentMicroServiceApplication.java
+++ src/main/java/com/rakesh/studentMicroService/StudentMicroServiceApplication.java
@@ -3,8 +3,10 @@ package com.rakesh.studentMicroService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"security","com"})
 public class StudentMicroServiceApplication {
 
 	public static void main(String[] args) {
